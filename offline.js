a374e16a81979bf0e3d924be60c7a3fd11f87f9b
GameMonster228 <92245414+GameMonster228@users.noreply.github.com>
2021-11-14 11:33:24 +0300

    first

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1636878730,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background.png",
+		"images/spaceship-sheet0.png",
+		"images/laser-sheet0.png",
+		"images/fire-sheet0.png",
+		"images/asteroid-sheet0.png",
+		"images/lose-sheet0.png",
+		"images/win-sheet0.png",
+		"images/explosion-sheet0.png",
+		"images/explosion-sheet1.png",
+		"images/shield-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
